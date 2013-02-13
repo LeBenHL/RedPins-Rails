125f75a8f312906d3bff3dd5155d0da96a68e224
Benjamin Le <kantas92@berkeley.edu>
2013-02-13 13:17:52 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedPins::Application.config.secret_token = '79ee737ca3cc78ffc7d16cd49a3eb774eaa01d840aa91e6bd8fa38e47cea387f2cf9d47dfe65f5dcdf556a27221d791211f7b2ae8d7060a184659be6c93d8b0a'
