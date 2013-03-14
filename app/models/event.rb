673cd7dcf5be774e1ec7151ccaf3ae078d87dacc
Benjamin Le <kantas92@berkeley.edu>
2013-03-14 14:11:47 -0700

    Specified user_id as the foreign key of the creator association

--- app/models/event.rb
+++ app/models/event.rb
@@ -19,7 +19,9 @@ class Event < ActiveRecord::Base
   validates :start_time, :presence => true
   validates :end_time, :presence => true
   validates :location, :presence => true
-  belongs_to :event_creator, :class_name => 'User'
+  validates :user_id, :presence => true
+  belongs_to :creator, :class_name => 'User', :foreign_key => "user_id"
+  validates :creator, :presence => true
   has_many :likes
   has_many :users, :through => :likes
   has_many :comments
