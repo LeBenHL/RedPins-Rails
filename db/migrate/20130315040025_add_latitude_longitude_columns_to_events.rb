b4f8d0537653d8446af421d9861f508ce200ba6b
Benjamin Le <kantas92@berkeley.edu>
2013-03-14 22:32:37 -0700

    Changed migration while to use float instead of double

--- db/migrate/20130315040025_add_latitude_longitude_columns_to_events.rb
+++ db/migrate/20130315040025_add_latitude_longitude_columns_to_events.rb
@@ -1,6 +1,6 @@
 class AddLatitudeLongitudeColumnsToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :latitude, :double
-    add_column :events, :longitude, :double
+    add_column :events, :latitude, :float
+    add_column :events, :longitude, :float
   end
 end
