6ee9fd41ac3e400b4ccc7697b3c7b28f1a222eb3
Benjamin Le <kantas92@berkeley.edu>
2013-03-20 23:18:12 -0700

    Fixed LAT/LONG of seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,34 @@
     @user5 = User.create(email: 'victorchang@gmail.com', facebook_id: 5, firstname: 'Victor', lastname: 'Chang')
 
     @event1 = Event.create(title: "Victor's Party", start_time: DateTime.new(2010,9,8), end_time: DateTime.new(2010,9,10),
-                          location: "2540 Regent St.", user_id: @user5.id, url: 'www.google.com', latitude: 36, longitude: -122)
+                          location: "2540 Regent St.", user_id: @user5.id, url: 'www.google.com', latitude: 37.86356, longitude: -122.25787)
 
     @event2 = Event.create(title: "Ben's Bash'", start_time: DateTime.new(2012,12,2), end_time: DateTime.new(2012,12,3),
-                           location: "2530 Hillegass Ave.", user_id: @user1.id, url: 'www.google.com', latitude: 36.01, longitude: -122.01)
+                           location: "2530 Hillegass Ave.", user_id: @user1.id, url: 'www.google.com', latitude: 37.86418, longitude: -122.25677)
 
     @event3 = Event.create(title: "Eric's BBQ'", start_time: DateTime.new(2013,3,13), end_time: DateTime.new(2013,3,14),
-                           location: "2520 College Ave.", user_id: @user4.id, url: 'www.google.com', latitude: 36.02, longitude: -122.02)
+                           location: "2520 College Ave.", user_id: @user4.id, url: 'www.google.com', latitude: 37.86483, longitude: -122.25420)
 
     @event4 = Event.create(title: "Andy's Picnic'", start_time: DateTime.new(2013,2,13), end_time: DateTime.new(2013,2,14),
-                           location: "2200 Fulton St..", user_id: @user3.id, url: 'www.google.com', latitude: 36.03, longitude: -122.03)
+                           location: "2200 Fulton St..", user_id: @user3.id, url: 'www.google.com', latitude: 37.86967, longitude: -122.26588)
 
     @event5 = Event.create(title: "Jerry's Lecture'", start_time: DateTime.new(2013,4,3), end_time: DateTime.new(2013,4,4),
-                           location: "2300 Oxford St..", user_id: @user2.id, url: 'www.google.com', latitude: 36.04, longitude: -122.04)
+                           location: "2300 Oxford St..", user_id: @user2.id, url: 'www.google.com', latitude: 37.86872, longitude: -122.26628)
 
     @event6 = Event.create(title: "Off The Grid", start_time: DateTime.new(2013,4,27), end_time: DateTime.new(2013,4,28),
-                           location: "2450 Haste St..", user_id: @user1.id, url: 'www.google.com', latitude: 36.05, longitude: -122.05)
+                           location: "2450 Haste St..", user_id: @user1.id, url: 'www.google.com', latitude: 37.86595, longitude: -122.25908)
 
     @event7 = Event.create(title: "Hippie Celebration", start_time: DateTime.new(2013,4,30), end_time: DateTime.new(2013,5,1),
-                           location: "2400 Bowditch Ave..", user_id: @user2.id, url: 'www.google.com', latitude: 36.01, longitude: -121.99)
+                           location: "2400 Bowditch Ave..", user_id: @user2.id, url: 'www.google.com', latitude: 37.86720, longitude: -122.25654)
 
     @event8 = Event.create(title: "Holi Party", start_time: DateTime.new(2013,1,11), end_time: DateTime.new(2013,1,12),
-                           location: "UC Berkeley.", user_id: @user3.id, url: 'www.google.com', latitude: 36.02, longitude: -121.98)
+                           location: "UC Berkeley.", user_id: @user3.id, url: 'www.google.com', latitude: 37.86948, longitude: -122.25969)
 
     @event9 = Event.create(title: "Danceworks Workshop", start_time: DateTime.new(2013,2,16), end_time: DateTime.new(2013,2,17),
-                           location: "Lower Sproul", user_id: @user4.id, url: 'www.google.com', latitude: 36.03, longitude: -121.97)
+                           location: "Lower Sproul", user_id: @user4.id, url: 'www.google.com', latitude: 37.86911, longitude: -122.26030)
 
     @event10 = Event.create(title: "Dead Poet's Society Meeting'", start_time: DateTime.new(2013,5,10), end_time: DateTime.new(2013,5,11),
-                           location: "2100 Durant Ave.", user_id: @user5.id, url: 'www.google.com', latitude: 36.04, longitude: -121.96)
+                           location: "2100 Durant Ave.", user_id: @user5.id, url: 'www.google.com', latitude: 37.86669, longitude: -122.26759)
 
     @user1.likeEvent(@event1.id, true)
     @user1.likeEvent(@event2.id, true)
